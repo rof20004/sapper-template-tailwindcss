3dc62b6b51b7384f9e5cbe495074b56cdd41a575
Rodolfo do Nascimento Azevedo <rodolfo@prodam.am.gov.br>
2019-10-11 08:06:48 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,55 @@
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	h2 {
+		text-align: center;
+		font-size: 2.8em;
+		margin-bottom: 50px;
+	}
+
+	figure {
+		margin: 0 auto;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 auto;
+		margin-bottom: 50px;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<h2><u>Svelte + Sapper + Tailwindcss</u></h2>
+
+<figure>
+	<img alt='Borat' src='great-success.png'>
+	<figcaption>HIGH FIVE!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
